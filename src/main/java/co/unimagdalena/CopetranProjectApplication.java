7f93a9b333ace8b308ed7c6265f718a851b01ee7
Esteban Puello <estebancamilop22@gmail.com>
2025-10-29 23:47:50 -0500

    Proyecto base Spring Boot inicial

---
+++ src/main/java/co/unimagdalena/CopetranProjectApplication.java
@@ -0,0 +1,11 @@
+package co.unimagdalena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopetranProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CopetranProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/co/unimagdalena/CopetranProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unimagdalena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopetranProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
