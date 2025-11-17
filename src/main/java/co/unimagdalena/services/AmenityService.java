6b220e48d22dfa4d7e85813c4e1a246b34cc0b5f
CSehuanes <Csehuanes@unimagdalena.edu.co>
2025-11-17 11:56:03 -0500

    Reubicacion de clase que estaba en el lugar equivocado

---
+++ src/main/java/co/unimagdalena/services/AmenityService.java
@@ -0,0 +1,16 @@
+package co.unimagdalena.services;
+
+import co.unimagdalena.api.dto.AmenityDto.*;
+
+import java.util.List;
+
+public interface AmenityService {
+
+    AmenityResponse createAmenity(AmenityCreateRequest request);
+    AmenityResponse updateAmenity(Long id, AmenityUpdateRequest request);
+    void deleteAmenity(Long id);
+
+    AmenityResponse getAmenityById(Long id);
+    List<AmenityResponse> getAllAmenities();
+}
+
