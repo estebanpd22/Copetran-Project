42380dbd7ccc01a987e56347da61e7f938484412
CSehuanes <Csehuanes@unimagdalena.edu.co>
2025-11-19 00:19:32 -0500

    Correccion de propiedad mal referenciada

--- src/main/java/co/unimagdalena/notification/NotificationHelper.java
+++ src/main/java/co/unimagdalena/notification/NotificationHelper.java
@@ -87,7 +87,7 @@ public class NotificationHelper {
                 ticket.getSeatNumber()
         );
 
-        String phone = ticket.getPassenger().getPhoneNumber();
+        String phone = ticket.getPurchase().getUser().getPhone();
 
         NotificationRequest request = new NotificationRequest(phone, message, type);
         notificationFactory.send(request);
